3ddd2ce3d6f90ba4aca434489f40a3cf7522df02
TakudzwaDavidMakumbe <takuclarity@gmail.com>
2024-11-01 14:03:11 +0200

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,8 +13,8 @@ function Header() {
             <img src={assets.group_profiles} alt="" className="w-28" />
             <p className="">Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' /> schedule your appointment hassle-free.</p>
         </div>
-        <a href="" className="">
-            Book appount <img src={assets.arrow_icon} alt="" className="" />
+        <a href="#speciality" className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300 ">
+            Book appount <img src={assets.arrow_icon} alt="w-3" className="" />
         </a>
       </div>
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctor from '../components/TopDoctor'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
+      <TopDoctor/>
     </div>
   )
 }
